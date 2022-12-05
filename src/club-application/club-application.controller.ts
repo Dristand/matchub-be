379c7af03559525dd494594426cc0e515daf140b
Jeremy Victor <jeremy.victor@ui.ac.id>
2022-12-05 16:58:05 +0700

    [Feature] Club Application Crud Adjustment

--- src/club-application/club-application.controller.ts
+++ src/club-application/club-application.controller.ts
@@ -1,4 +1,11 @@
 import { Controller } from '@nestjs/common';
+import {CrudController} from "@nestjsx/crud";
+import {ClubApplication} from "./club-application.entity";
+import {ClubApplicationService} from "./club-application.service";
 
 @Controller('club-application')
-export class ClubApplicationController {}
+export class ClubApplicationController implements CrudController<ClubApplication> {
+    constructor(public service: ClubApplicationService) {
+
+    }
+}
--- src/club-application/club-application.service.ts
+++ src/club-application/club-application.service.ts
@@ -1,4 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import {TypeOrmCrudService} from "@nestjsx/crud-typeorm";
+import {ClubApplication} from "./club-application.entity";
+import {InjectRepository} from "@nestjs/typeorm";
 
 @Injectable()
-export class ClubApplicationService {}
+export class ClubApplicationService extends TypeOrmCrudService<ClubApplication> {
+    constructor(@InjectRepository(ClubApplication) repo) {
+        super(repo);
+    }
+}
